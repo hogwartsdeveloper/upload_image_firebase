7a0e07d0679ad05161101f50acf1a29fca88b6df
Zhannur <jannuraidynuly@gmail.com>
2022-02-07 10:13:42 +0500

    upload image firebase

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
 import { Button, StyleSheet, View } from 'react-native';
-import { launchImageLibrary } from "react-native-image-picker";
+import * as ImagePicker from "expo-image-picker";
+import firebaseConfig from './firebaseConfig';
+import { initializeApp } from "firebase/app"
+import { getStorage, ref, uploadBytes } from "firebase/storage"
+
+initializeApp(firebaseConfig);
 
 export default function App() {
+
+  const openGallery = async() => {
+      let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1
+      });
+
+      if (!result.cancelled) {
+        const storage = getStorage();
+        const refer = ref(storage, result.uri.substr(result.uri.lastIndexOf('/') + 1))
+
+        const img = await fetch(result.uri);
+        const bytes = await img.blob();
+
+
+        await uploadBytes(refer, bytes)
+      }
+
+  }
+
+
   return (
     <View style={styles.container}>
-        <Button title='Upload image' />
+        <Button title='Upload image' onPress={openGallery}/>
     </View>
   );
 }
---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+export default {
+
+  apiKey: "AIzaSyD90Ges5g0rWENIMBQd363Q4GehNxVLJuI",
+
+  authDomain: "animal-pedia.firebaseapp.com",
+
+  projectId: "animal-pedia",
+
+  storageBucket: "animal-pedia.appspot.com",
+
+  messagingSenderId: "481105680294",
+
+  appId: "1:481105680294:web:146755a98fbcdada512167",
+
+  measurementId: "G-0ZQD2HEFH9"
+
+};
